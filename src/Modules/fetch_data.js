86b788de828c71e80f8cab08e72666db2fe2dcdf
santa <santhosh.003z@gmail.com>
2024-10-16 23:33:01 +0530

    add fetch weather API and Geo API

--- src/Modules/fetch_data.js
+++ src/Modules/fetch_data.js
@@ -1,5 +1,39 @@
+const axios = require(`axios`);
 
+const fetchData = async function(lat,lon){
+    try {
+        const response = await axios(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,is_day,precipitation,weather_code,wind_speed_10m&hourly=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timeformat=unixtime`);
+        const currentData = response.data.current;
+        const hourlyData = response.data.hourly;
+        const dailyData = response.data.daily;
+        return {currentData, hourlyData, dailyData};
+    } catch (error) {
+        alert(`Error, Try again`);alert(`Error, Try again`);
+    }
+}
 
-const fetchData = async function(){
+const covertCity = async function(cityName){
+    try {
+        const config = {headers:{ 'x-api-key': '3PkatoaXRFVsNBlIIy8qSA==oBlz0frN5OttAIKr'}};
+        const response = await axios(`https://api.api-ninjas.com/v1/geocoding?city=${cityName}`, config);
+        const city = response.data[0];
+        return city;
+    } catch (error) {
+        alert(`Error, Try again`);
+    }
+}
+
+const updateDate = function(){
+
+}
+
+const updateWeatherText = function(){
     
 }
+
+const updateWeatherIcon = function(){
+    
+}
+
+
+export {fetchData,covertCity}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-// `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,is_day,precipitation,weather_code,wind_speed_10m&hourly=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timeformat=unixtime`
+import "./output.css"
+import { fetchData,covertCity } from "./modules/fetch_data.js";
+
+let cityName = `chennai`;
+const elements = {
+    city: document.querySelector(`.city`),
+    currentDate: document.querySelector(`.current-date`),
+    currentTemp: document.querySelector(`.current-temp`),
+    currentTempText: document.querySelector(`.current-temp-text`),
+}
+
+function updateDom(weather,city){
+    elements.city.textContent = city.name.toUpperCase();
+    elements.currentTemp.textContent = `${weather.currentData.temperature_2m}°`;
+}
+
+
+
+async function getData() {
+    const data = await covertCity(cityName);
+    const weather = await fetchData(data.latitude,data.longitude);
+    updateDom(weather,data);
+}  
+
+
+const searchBtn = document.querySelector(`.search`);
+
+searchBtn.addEventListener(`click`, ()=>{
+    const input = prompt(`Enter a city name`);
+    cityName = input.toLowerCase();
+    getData(cityName);
+})
+
+// currentData{
+// is_day: 0,
+// precipitation: 0,
+// relative_humidity_2m: 94,
+// temperature_2m: 19.3,
+// weather_code: 1,
+// wind_speed_10m: 5.1,
+// }
